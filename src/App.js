bf313c75608852cfcd64ffe4e2d7fd47dab80018
Luis <luisrc91@live.com>
2023-04-04 23:38:21 -0400

    added todos

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Counter from './components/Counter'
 import './App.css';
+import Todo from './Todo'
 
 function App() {
   return (
     <div className="App">
       <Counter />
+      <Todo />
     </div>
   );
 }
